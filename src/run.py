97f6300be01910e6cc3fcc1689e4d54320547a72
AmirCodeStar <amirghodsi9651@gmail.com>
2025-12-18 23:37:17 +0330

    first file uplude

--- src/run.py
+++ src/run.py
@@ -1,3 +1,23 @@
 import streamlit as st
+import json
+import seaborn as sns
+import numpy as np
+
+try:
+    from StringIo import StringIO
+except ImportError:
+    from io import StringIO
 
 st.title("🖥pytopia-dashboard")
+
+
+with st.expander('statistics'):
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+
+        data = json.load(uploaded_file)
+        st.json(data)
+
